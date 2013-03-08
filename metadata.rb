0e1b1c1d0dcb013d449a0521fbc1c6c68e075310
Ruslan Pilin <dohque@gmail.com>
2013-03-08 19:42:49 +0400

    Sekeleton for recipe

---
+++ metadata.rb
@@ -0,0 +1,9 @@
+maintainer       "PilinSolutions"
+maintainer_email "dohque@gmail.com"
+license          "Apache License"
+description      "Installs/Configures liferay_war"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.1.0"
+
+depends 'mysql'
+
---
+++ recipes/default.rb
@@ -0,0 +1,8 @@
+#
+# Cookbook Name:: reviewboard_cookbook
+# Recipe:: default
+#
+# Copyright 2013, PilinSolutions
+#
+
+
